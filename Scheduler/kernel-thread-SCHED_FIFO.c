60bfd556caf3c93f7a45822c3d972e425744230e
Raj Kumar Sanpui <raj.kumar.sanpui@gmail.com>
2020-12-27 23:56:22 +0530

    Update kernel-thread-SCHED_FIFO.c

--- Scheduler/kernel-thread-SCHED_FIFO.c
+++ Scheduler/kernel-thread-SCHED_FIFO.c
@@ -8,64 +8,24 @@
 #include <linux/kthread.h>          //used for kthread_create
 #include <linux/delay.h>            //used for ssleep()
 
-/*@brief
- * variables for this example, task structs, cpu id's as integer
-*/
-
 static struct task_struct *worker_task,*default_task;
 static int get_current_cpu,set_current_cpu;
 #define WORKER_THREAD_DELAY 4
 #define DEFAULT_THREAD_DELAY 6
 
-/*@brief
- * Linux Kernel Module Parameters
-*/
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Raj Kumar Sanpui");
-MODULE_DESCRIPTION("Kernel thread example");
-MODULE_ALIAS("Threading");
 
-
-/*@brief  worker task main constructor
- *@note   it will forever after while() loop
- *@params arguments from when task creating with kthread_creat()
- *@return nothing
-*/
 static int worker_task_handler_fn(void *arguments)
 {
-        /*@brief
-         * here is your world, you can declare your variables,structs
-        */
-
-        /* @note this macro will allow to stop thread from userspace
-         * or kernelspace
-         */
         allow_signal(SIGKILL);
 
-        /*@attention while(true),while(1==1),for(;;) loops will can't
-         *receive signal for stopping thread
-         */
         while(!kthread_should_stop()){
                 printk("Worker thread executing on system CPU:%d \n",get_cpu());
                 ssleep(WORKER_THREAD_DELAY);
 
-        /*@attention while(true),for(;;) loops will can't receive signals,
-     *kernel threads doesnt allow sync signal handling like user space apps.
-         *you must check signals in forever loops everytime
-         *if signal_pending function capture SIGKILL signal, then thread will exit
-         */
 
                 if (signal_pending(worker_task))
                                     break;
         }
-        /*@brief
-         * do_exit is same as exit(0) function, but must be used with threads
-         */
-
-        /*@brief
-         * Kernel Thread has higher priority than user thread because Kernel threads
-         * are used to provide privileged services to applications.
-         */
 
         do_exit(0);
 
@@ -73,36 +33,15 @@ static int worker_task_handler_fn(void *arguments)
         return 0;
 }
 
-/*@brief   default task main constructor
- *@attention this task have highest real time priority
- *@note   it will forever after while() loop
- *@params arguments from when task creating with kthread_creat()
- *@return nothing
-*/
 static int default_task_handler_fn(void *arguments)
 {
-        /*@brief
-         * here is your world, you can declare your variables,structs
-        */
-
-        /* @note this macro will allow to stop thread from userspace
-         * or kernelspace
-         */
         allow_signal(SIGKILL);
 
-        /*@attention while(true),while(1==1),for(;;) loops will can't
-     *receive signal for stopping thread
-        */
         while(!kthread_should_stop())
         {
                 printk("Default thread executing on system CPU:%d \n",get_cpu());
                 ssleep(DEFAULT_THREAD_DELAY);
 
-        /*@attention while(true),for(;;) loops will can't receive signals,
-         *kernel threads doesnt allow sync signal handling like user space apps.
-         *you must check signals in forever loops everytime
-         *if signal_pending function capture SIGKILL signal, then thread will exit
-         */
 
             if (signal_pending(default_task))
                             break;
@@ -110,24 +49,14 @@ static int default_task_handler_fn(void *arguments)
 
         printk("Default task exiting\n");
 
-        /*@brief
-         * do_exit is same as exit(0) function, but must be used with threads
-        */
         do_exit(0);
 
         return 0;
 }
-/*@brief linux kernel module initial macro function
- *@attention only run one once when module installed
- *@note   must be exit with return
- *@params nothing
- *@return signal status with int parameter
-*/
+
 static int __init kernel_thread_init(void)
 {
 
-        /*@brief scheduler priority structs to set task priority
-         */
         struct sched_param task_sched_params =
         {
                         .sched_priority = MAX_RT_PRIO
@@ -138,15 +67,9 @@ static int __init kernel_thread_init(void)
         printk("Initializing kernel mode thread example module\n");
         printk("Creating Threads\n");
 
-        /*@brief get current cpu to bind over task
-         */
         get_current_cpu = get_cpu();
-        printk("Getting current CPU %d to binding worker thread\n",
-                                                                                        get_current_cpu);
+        printk("Getting current CPU %d to binding worker thread\n",get_current_cpu);
 
-        /*@brief initialize worker task with arguments, thread_name and cpu
-         *@note you can see thread name using ps aux command on userspace
-         */
         worker_task = kthread_create(worker_task_handler_fn,
                         (void*)"arguments as char pointer","thread_name");
         kthread_bind(worker_task,get_current_cpu);
@@ -156,54 +79,37 @@ static int __init kernel_thread_init(void)
         else
                 printk("Worker task error while creating\n");
 
-        /*@brief initialize default task with arguments,thread_name and another cpu
-         *@note you can see thread name using ps aux command on userspace
-        */
         set_current_cpu = 2;
-        printk("Getting current CPU %d to binding default thread\n",
-                                                                                        set_current_cpu);
+        printk("Getting current CPU %d to binding default thread\n",set_current_cpu);
 
         default_task = kthread_create(default_task_handler_fn,
                                 (void*)"arguments as char pointer","thread_name");
         kthread_bind(default_task,set_current_cpu);
 
-        /*@brief set scheduler priority to default task
-        */
+        // Assign a scheduler SCHED_FIFO = RT scheduler
         sched_setscheduler(default_task, SCHED_FIFO, &task_sched_params);
 
-        /*@brief tasks are now process, start them
-        */
         wake_up_process(worker_task);
         wake_up_process(default_task);
 
-        /*@brief check task if they are started succesfully
-        */
         if(worker_task)
                 printk("Worker thread running\n");
         else
                 printk("Worker task can't start\n");
 
-        if(default_task)
+                if(default_task)
                 printk("Default thread running\n");
         else
                 printk("Default task can't start\n");
 
         return 0;
 }
-/*@brief linux kernel module deinitial macro function
- *@attention only run one once when module removed
- *@note   must be exit with return
- *@params nothing
- *@return signal status with int parameter
-*/
+
 static void __exit kernel_thread_exit(void)
 {
 
         printk("Module removing from kernel, threads stopping\n");
 
-        /*@brief
-         * this functions will send SIGKILL's to stop threads when module removing
-        */
 
         if(worker_task)
                 kthread_stop(worker_task);
@@ -219,8 +125,11 @@ static void __exit kernel_thread_exit(void)
 
 }
 
-/*@brief
- * set initial and exit callback functions to module macro
-*/
 module_init(kernel_thread_init);
 module_exit(kernel_thread_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Raj Kumar Sanpui");
+MODULE_DESCRIPTION("Kernel thread example");
+MODULE_ALIAS("Threading");
+
