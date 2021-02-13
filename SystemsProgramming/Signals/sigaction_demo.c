67fdc83822331667cef0a6f401afb27d2f004774
Raj Kumar Sanpui <raj.kumar.sanpui@gmail.com>
2021-02-13 20:55:30 +0530

    Update sigaction_demo.c

--- SystemsProgramming/Signals/sigaction_demo.c
+++ SystemsProgramming/Signals/sigaction_demo.c
@@ -25,7 +25,7 @@ int main()
    sa.sa_sigaction => Pointer to signal catching function with multiple args.
                  void(*) (int, siginfo_t *, void *)
                  
-   sigaction(SIGINT, &sa, NULL) => If the last argument is not NULL, it stores the current signal actions in this set.
+   sigaction(SIGNAL_NUMBER, &sa_new, &sa_old) => If the last argument is not NULL, it stores the current signal actions in this set.
 
    https://pubs.opengroup.org/onlinepubs/007904875/functions/sigaction.html
 */
