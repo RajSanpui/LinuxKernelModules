f877b60028b5646d01e4642204a3ee3091c07b7b
Raj Kumar Sanpui <raj.kumar.sanpui@gmail.com>
2021-02-13 21:18:00 +0530

    Update simple.c

--- SystemsProgramming/pthreads/simple.c
+++ SystemsProgramming/pthreads/simple.c
@@ -1,18 +1,22 @@
-include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
 
 void *myThreadFun(void *vargp)
 {
-        printf("Inside thread: Argument recvd %s",(char*)vargp);
+        printf("Inside thread: I am %s",(char*)vargp);
 }
 
 int main()
 {
-        pthread_t tid;
+        pthread_t tid1, tid2;
 
-        pthread_create(&tid, NULL, myThreadFun, "Hello Thread\n");
-        pthread_join(tid, NULL);
+        pthread_create(&tid1, NULL, myThreadFun, "Thread1\n");
+        pthread_create(&tid2, NULL, myThreadFun, "Thread2\n"); // The last argument is optional can be NULL
+        
+        pthread_join(tid11, NULL);
+        pthread_join(tid12, NULL);
+        
         exit(0);
 }
